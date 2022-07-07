6e5abd9b347a13428739cb00989d8881f7addee6
Your Name <you@example.comls>
2022-07-07 00:29:48 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabet 10 times then a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	int n;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (n = 'a'; n <= 'n'; n++)
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+	}
+}
